9902ea4446afd61948048d4c2b724c8117bd7988
Xander-Leatherwood-TU <tuq62657@temple.edu>
2024-08-28 10:17:59 -0400

    added random.c

---
+++ random.c
@@ -0,0 +1,20 @@
+// randchar goes here
+
+// program steps:
+// - randomly generate a number within range of integers 65-90
+//      that correspond to an ASCII letter value ('A'-'Z')
+// - convert ASCII value to character
+// - return character 
+
+
+char randchar()
+{
+    char randomLetter;
+
+    // random number generation
+
+    // convert number to char
+
+    return randomLetter;
+}
+
